daf6ee55f3de8514de8d2aa6c17578ba7c906b60
Gcraft13 <120202358+Gcraft13@users.noreply.github.com>
2025-08-25 14:24:25 -0400

    add function for getPlayerChoice and getComputerChoice

--- RPS.js
+++ RPS.js
@@ -1,1 +1,35 @@
-alert("Hello World!");
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+
+  switch (randomNumber) {
+    case 0:
+      computerChoice = "Rock";
+      // Code to execute if expression === value1
+      break;
+    case 1:
+      computerChoice = "Scissors";
+      // Code to execute if expression === value2
+      break;
+    // ... more cases
+    case 2:
+      computerChoice = "Paper";
+      // Code to execute if expression === value2
+      break;
+    default:
+      console.log("please pick a weapon!");
+  }
+  console.log(computerChoice);
+}
+
+function getPlayerChoice() {
+  let playerChoice = prompt(
+    "Please select your weapon: rock, scissors, or paper"
+  );
+  if (playerChoice != "") {
+    alert("Please select a weapon");
+  }
+  console.log(playerChoice);
+}
+
+getPlayerChoice();
+getComputerChoice();
